f79ead07c7e46e2662d7ec0099565c163440a8b5
hyeon9811 <it.hyeon98@contentlap.com>
2023-04-25 11:58:11 +0900

    var, let, const

---
+++ Javascript/var, let, const/var, let, const.js
@@ -0,0 +1,38 @@
+// var
+/*
+var greeting = 'hello';
+console.log(greeting); // hello
+
+var greeting = 'hi';
+console.log(greeting); // hi
+
+greeting = 'how are you';
+console.log(greeting); // how are you
+*/
+
+// let
+/*
+let greeting = 'hello';
+console.log(greeting); // hello
+
+let greeting = 'hi';
+console.log(greeting); 
+// caught SyntaxError: Identifier 'greeting' has already been declared (
+
+greeting = 'how are you';
+console.log(greeting); // how are you
+*/
+
+// const
+/*
+const greeting = 'hello';
+console.log(greeting); // hello
+
+const greeting = 'hi';
+console.log(greeting); 
+// Uncaught SyntaxError: Identifier 'greeting' has already been declared (
+
+greeting = 'how are you';
+console.log(greeting); 
+// Uncaught TypeError: Assignment to constant variable
+*/
