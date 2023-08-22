83984cef84bef4f1c47e529674fd273297cb0009
ehdgusdl9177 <k23129177@gmail.com>
2023-08-22 16:20:37 +0900

    refreshToken 생성

--- EXPRESS-AUTH-APP/index.js
+++ EXPRESS-AUTH-APP/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const req = require("express/lib/request");
 const { JsonWebTokenError } = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const secretText = "superSecret";
+const refreshSecretText = "superSecret";
 
 const posts = [
   {
@@ -17,14 +19,29 @@ const posts = [
   },
 ];
 
+let refreshTokens = [];
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
 
   // jwt를 이용해서 토큰 생성하기  payload + secretText
-  const accessToken = jwt.sign(user, secretText);
+  // 유효기간 추가
+  const accessToken = jwt.sign(user, secretText, { expiresIn: "30s" });
+
+  // jwt를 이용해서 refreshToken도 생성
+  const refreshToken = jwt.sign(user, refreshSecretText, { expiresIn: "1d" });
+  refreshTokens.push(refreshToken);
+
+  // refreshToken을 쿠키에 넣어주기
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
   res.json({ accessToken: accessToken });
 });
 
@@ -46,6 +63,27 @@ function authMiddleware(req, res, next) {
     next();
   });
 }
+
+app.get("/refresh", (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(403);
+
+  const refreshToken = cookies.jwt;
+  // refreshToken이 데이터베이스에 있는 토큰인지 확인
+  if (!refreshToken.includes(refreshToken)) {
+    return res.sendStatus(403);
+  }
+
+  // token이 유효한 토큰인지 확인
+  jwt.verify(refreshToken, refreshSecretText, (err, user) => {
+    if (err) return res.sendStatus(403);
+    // accessToken을 생성하기
+    const accessToken = jwt.sign({ name: user.name }, secretText, {
+      expiresIn: "30s",
+    });
+    res.json({ accessToken });
+  });
+});
 
 const port = 4000;
 
