938c0e490f5300ae2e104d9e2aeb3dcaa53d7f62
ehdgusdl9177 <k23129177@gmail.com>
2023-08-22 14:56:28 +0900

    토큰을 이용해서 요청 보내기

--- EXPRESS-AUTH-APP/index.js
+++ EXPRESS-AUTH-APP/index.js
@@ -28,9 +28,24 @@ app.post("/login", (req, res) => {
   res.json({ accessToken: accessToken });
 });
 
-app.get("/posts", (req, res) => {
+app.get("/posts", authMiddleware, (req, res) => {
   res.json(posts);
 });
+
+function authMiddleware(req, res, next) {
+  // 토큰을 request headers에서 가져오기
+  const authHeader = req.headers["authorization"];
+  // Bearer asdasdasdasad
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  // 토큰이 있으니 유효한 토큰인지 확인
+  jwt.verify(token, secretText, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 const port = 4000;
 
