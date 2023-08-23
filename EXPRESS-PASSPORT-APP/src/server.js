7f31203ac6351f484fd21488321a7fb9238e1926
ehdgusdl9177 <k23129177@gmail.com>
2023-08-23 17:15:35 +0900

    로그아웃 기능 구현

--- EXPRESS-PASSPORT-APP/src/server.js
+++ EXPRESS-PASSPORT-APP/src/server.js
@@ -87,6 +87,15 @@ app.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
+app.post("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
+});
+
 app.get("/signup", checkNotAuthenticated, (req, res, next) => {
   res.render("signup");
 });
