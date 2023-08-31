84972d348d866c68d7a7c8a694f57ca3a072a3c7
ehdgusdl9177 <k23129177@gmail.com>
2023-08-31 15:59:06 +0900

    TypeOrm

---
+++ ORM/Typeorm.md
@@ -0,0 +1,13 @@
+## Typeorm
+
+- Typeorm은 node.js에서 실행되고 TypeScript로 작성된 객체 관계형 매퍼 라이브러리 이다.
+- TypeORM은 MySQL, PostgreSQL, MariaDB, SQLite, MS SQL Server, Oracle, SAP Hana 및 WebSQL과 같은 여러 데이터베이스를 지원한다.
+
+### TypeORM 특징과 이점
+
+- 모델을 기반으로 데이터베이스 데이블 체계를 자동을 생성한다.
+- 데이터베이스에서 개체를 쉽게 삽입, 업데이트 및 삭제할 수 있다.
+- 테이블 간의 매핑 (일대일, 일대다 및 다대다)을 만든다.
+- 간단한 CLI 명령을 제공한다
+- TypeORM은 간단한 코딩으로 ORM 프레임 워크를 사용하기 쉽다.
+- TpyeORM은 다른 모듈과 쉽게 통합된다.
---
+++ typeorm-app/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import { AppDataSource } from "./data-source";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("running");
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server Running at http://localhost:${port}`);
+});
