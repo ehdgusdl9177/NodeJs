f9e9b5791b1b4bd3600c382644eb679735211e51
ehdgusdl9177 <k23129177@gmail.com>
2023-08-31 11:50:36 +0900

    mongoose CRUD(UPDATE, DELETE)구현

--- EXPRESS-PASSPORT-APP/src/controllers/products.controller.js
+++ EXPRESS-PASSPORT-APP/src/controllers/products.controller.js
@@ -31,8 +31,42 @@ const getProductById = async (req, res, next) => {
   }
 };
 
+const updateProduct = async (req, res, next) => {
+  try {
+    let updatedProduct = await productModel.findByIdAndUpdate(
+      req.params.productId,
+      req.body,
+      { new: true }
+    );
+    if (updatedProduct) {
+      res.status(200).json(updatedProduct);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteProduct = async (req, res, next) => {
+  try {
+    let deletedProduct = await productModel.findByIdAndDelete(
+      req.params.productId
+    );
+    if (deletedProduct) {
+      res.status(200).json(deletedProduct);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   createProduct,
   getProducts,
   getProductById,
+  updateProduct,
+  deleteProduct,
 };
