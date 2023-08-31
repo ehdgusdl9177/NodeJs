024581c3f560c1b272443226576412bb1421fb9e
ehdgusdl9177 <k23129177@gmail.com>
2023-08-31 11:43:19 +0900

    mongoose CRUD(READ)구현

--- EXPRESS-PASSPORT-APP/src/controllers/products.controller.js
+++ EXPRESS-PASSPORT-APP/src/controllers/products.controller.js
@@ -8,4 +8,31 @@ const createProduct = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = createProduct;
+
+const getProducts = async (req, res, next) => {
+  try {
+    const allProducts = await productModel.find({});
+    res.status(200).json(allProducts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getProductById = async (req, res, next) => {
+  try {
+    const product = await productModel.findById(req.params.productId);
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createProduct,
+  getProducts,
+  getProductById,
+};
--- EXPRESS-PASSPORT-APP/src/routes/products.router.js
+++ EXPRESS-PASSPORT-APP/src/routes/products.router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const productsController = require("../controllers/products.controller");
 const productsRouter = express();
 
-productsRouter.post("/", productsController.createdProduct);
+productsRouter.post("/", productsController.createProduct);
+productsRouter.get("/", productsController.getProducts);
+productsRouter.get("/:productId", productsController.getProductById);
 
 module.exports = productsRouter;
