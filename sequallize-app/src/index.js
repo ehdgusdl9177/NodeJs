272611b8c1f931a09a312335d4d837f0cabf9458
ehdgusdl9177 <k23129177@gmail.com>
2023-09-04 13:41:13 +0900

    Sequelize를 이용한 CRUD 구현

--- sequallize-app/src/index.js
+++ sequallize-app/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { get } = require("express/lib/response");
 const db = require("./models");
 const app = express();
 
@@ -26,6 +27,80 @@ app.post("/users", (req, res) => {
     .catch((err) => {
       res.status(500).send({
         message: err.message || "유저를 생성하는데 오류가 발생했습니다",
+      });
+    });
+});
+
+app.get("/users", (req, res) => {
+  User.findAll()
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `유저 정보를 가져오는 데 실패 했습니다.`,
+      });
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  User.findByPk(id)
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({
+          message: `id가 ${id}인 유저가 없습니다.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `${id}인 유저를 찾는데 에러가 났습니다`,
+      });
+    });
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  User.update(req.body, {
+    where: {
+      id,
+    },
+  })
+    .then((result) => {
+      if (result[0] === 1) {
+        res.send("성공했습니다.");
+      } else {
+        res.send(`${id}에 맞는 유저가 없습니다.`);
+      }
+    })
+    .catch((err) => {
+      message =
+        err.message || `${id}인 유저를 업데이트 하는데 에러가 났습니다.`;
+    });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  User.destroy({
+    where: {
+      id,
+    },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: `유저가 성공적으로 삭제되었습니다.`,
+        });
+      } else {
+        res.send(`${id} 유저를 찾지 못했습니다.`);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `${id} 유저를 삭제하는데 실패했습니다.`,
       });
     });
 });
