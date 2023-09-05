37a9706cb251b495c8c2dc354b9e888c27ef865d
ehdgusdl9177 <k23129177@gmail.com>
2023-09-05 14:28:28 +0900

    Resolver 모듈화

--- EXPRESS-GRAPHQL/comments/comments.model.js
+++ EXPRESS-GRAPHQL/comments/comments.model.js
@@ -1,7 +1,15 @@
-module.exports = [
+const comments = [
   {
     id: "comment1",
     text: "It is a first comment",
     likes: 1,
   },
 ];
+
+const getAllComments = () => {
+  return comments;
+};
+
+module.exports = {
+  getAllComments,
+};
--- EXPRESS-GRAPHQL/server.js
+++ EXPRESS-GRAPHQL/server.js
@@ -3,33 +3,26 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const path = require("path");
 const app = express();
 
 const loadedTypes = loadFilesSync("**/*", {
   extensions: ["graphql"],
 });
 
-const schema = makeExecutableSchema({
-  typeDefs: [loadedTypes],
-  resolvers: {
-    Query: {
-      posts: (parent, args, context, info) => {
-        return parent.posts;
-      },
-    },
-  },
-});
+const loadedResolvers = loadFilesSync(
+  path.join(__dirname, "**/*.resolvers.js")
+);
 
-const root = {
-  posts: require("./posts/posts.model"),
-  comments: require("./comments/comments.model"),
-};
+const schema = makeExecutableSchema({
+  typeDefs: loadedTypes,
+  resolvers: loadedResolvers,
+});
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 );
